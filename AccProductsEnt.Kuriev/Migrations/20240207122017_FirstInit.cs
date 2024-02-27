088f777f11c8d4c87b17762497099d9bb5d1b342
NikitaKuriev <nkuriev@gmail.com>
2024-02-27 09:59:24 +0300

    Revert "Revert "+Slayder""
    This reverts commit 11710584d83136c5fe858c147b67cecfda421194.

---
+++ AccProductsEnt.Kuriev/Migrations/20240207122017_FirstInit.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AccProductsEnt.Kuriev.Migrations
+{
+    public partial class FirstInit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
