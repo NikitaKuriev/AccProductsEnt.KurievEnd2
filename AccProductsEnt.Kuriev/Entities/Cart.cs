6d8fa3145b11cf18c1877330950bae12d681941b
NikitaKuriev <nkuriev@gmail.com>
2024-02-27 09:34:10 +0300

    +Slayder

---
+++ AccProductsEnt.Kuriev/Entities/Cart.cs
@@ -0,0 +1,25 @@
+﻿using AccProductsEnt.Kuriev.Pages;
+using Microsoft.AspNetCore.Razor.Language;
+
+namespace AccProductsEnt.Kuriev.Entities
+{
+    //public class Cart : ProductCartModel
+    //{
+    //    private List<ProductCartModel> ProductCollection = new List<ProductCartModel>();
+
+    //    public void AddItem (Product product, int quantity)
+    //    { 
+    //        ProductCartModel line = ProductCollection 
+    //            .Where(p => p.ProductName.ProductId == product.ProductId)
+    //            .FirstOrDefault();
+
+
+    //        if (line == null)
+    //        {
+    //            ProductCollection.Add(new ProductCartModel { });
+    //        }
+    //    }
+
+
+    //}
+}
